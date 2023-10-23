26ec32986255de44406b2af592718806adf719a1
Mario5346 <mapg555@gmail.com>
2023-10-23 19:30:02 -0400

    handshake testing class

--- handshake.java
+++ handshake.java
@@ -1,14 +1,16 @@
 package CNT4007_Project_Fall_2023;
 import java.net.*;
 import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 import java.io.*;
 
 public class handshake {
     byte[] header;
     int size = 32;
-    int peerID;
+    int peerID= 69;
     byte[] hs = new byte[32];
-    public byte[] handshake(int ID){
+    
+    public handshake(int ID){
         header = new String("P2PFILESHARINGPROJ").getBytes();
         byte[] zeroes = new String("0000000000").getBytes();
         byte[] id = ByteBuffer.allocate(4).putInt(ID).array();
@@ -17,14 +19,21 @@ public class handshake {
             hs[i] = header[i];
             currentByte++;
         }
+        int start_next = currentByte;
         for(int i = 0; i < 10; i++){
-            hs[i+currentByte] = zeroes[i];
+            hs[i+start_next] = zeroes[i];
             currentByte++;
         }
+        start_next = currentByte;
         for(int i = 0; i < 4; i++){
-            hs[i+currentByte] = id[i];
+            hs[i+start_next] = id[i];
             currentByte++;
         }
-        return hs;
+    }
+    public static void main(String[] args){
+        handshake s = new handshake(69);
+        String p = new String(s.hs, StandardCharsets.UTF_8);
+        System.out.println(p); //
+    
     }
 }
