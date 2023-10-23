265203eae5fea535aa0dbdc101d626604fc81dbd
Mario5346 <mapg555@gmail.com>
2023-10-23 19:17:08 -0400

    created handshake message

--- handshake.java
+++ handshake.java
@@ -1,12 +1,30 @@
 package CNT4007_Project_Fall_2023;
 import java.net.*;
+import java.nio.ByteBuffer;
 import java.io.*;
 
 public class handshake {
-    String header;
-    int size;
+    byte[] header;
+    int size = 32;
     int peerID;
-    public void handshakeMessage(){
-        
+    byte[] hs = new byte[32];
+    public byte[] handshake(int ID){
+        header = new String("P2PFILESHARINGPROJ").getBytes();
+        byte[] zeroes = new String("0000000000").getBytes();
+        byte[] id = ByteBuffer.allocate(4).putInt(ID).array();
+        int currentByte = 0;
+        for(int i = 0; i < 18; i++){
+            hs[i] = header[i];
+            currentByte++;
+        }
+        for(int i = 0; i < 10; i++){
+            hs[i+currentByte] = zeroes[i];
+            currentByte++;
+        }
+        for(int i = 0; i < 4; i++){
+            hs[i+currentByte] = id[i];
+            currentByte++;
+        }
+        return hs;
     }
 }
