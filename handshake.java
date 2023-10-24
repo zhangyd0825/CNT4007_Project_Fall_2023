ee72216f6a5fdda96926ceaf03ab01b2e5cb855d
Mario5346 <mapg555@gmail.com>
2023-10-24 13:41:09 -0400

    handshake message class

--- handshake.java
+++ handshake.java
@@ -7,33 +7,34 @@ import java.io.*;
 public class handshake {
     byte[] header;
     int size = 32;
-    int peerID= 69;
-    byte[] hs = new byte[32];
+    byte[] peerID;
+    byte[] message = new byte[32];
     
     public handshake(int ID){
         header = new String("P2PFILESHARINGPROJ").getBytes();
         byte[] zeroes = new String("0000000000").getBytes();
-        byte[] id = ByteBuffer.allocate(4).putInt(ID).array();
+        peerID = ByteBuffer.allocate(4).putInt(ID).array();
+        System.out.println("PeerID: " + ByteBuffer.wrap(peerID).getInt());
         int currentByte = 0;
         for(int i = 0; i < 18; i++){
-            hs[i] = header[i];
+            message[i] = header[i];
             currentByte++;
         }
         int start_next = currentByte;
         for(int i = 0; i < 10; i++){
-            hs[i+start_next] = zeroes[i];
+            message[i+start_next] = zeroes[i];
             currentByte++;
         }
         start_next = currentByte;
         for(int i = 0; i < 4; i++){
-            hs[i+start_next] = id[i];
+            message[i+start_next] = peerID[i];
             currentByte++;
         }
     }
     public static void main(String[] args){
-        handshake s = new handshake(69);
-        String p = new String(s.hs, StandardCharsets.UTF_8);
-        System.out.println(p); //
+        handshake s = new handshake(1001);
+        String p = new String(s.message, StandardCharsets.UTF_8);
+        System.out.println(p);
     
     }
 }
